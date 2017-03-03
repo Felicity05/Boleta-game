f1084a58f2388ed073760f7737138197183da53a
Felicity05 <arelysalvarezpadilla@Arelyss-MacBook-Air.local>
2017-03-02 23:30:20 -0500

    new changes

--- mySixthAssignment/src/NumbersGame.java
+++ mySixthAssignment/src/NumbersGame.java
@@ -9,28 +9,66 @@
  * @author arelysalvarezpadilla
  */
 public class NumbersGame {
-    
-   //instance variables  
+
+    //instance variables  
     private String betType;
     private double betAmount;
     private int playerNumbers;
     private int winningNumber;
-    
-    public NumbersGame(String betType, double betAmount, int playerNumbers, int winningNumber)
-    {
+    private double amountEarned;
+
+    public NumbersGame(String betType, double betAmount, int playerNumbers, int winningNumber) {
         this.betType = betType;
         this.betAmount = betAmount;
         this.playerNumbers = playerNumbers;
         this.winningNumber = winningNumber;
     }
-   
+
     //returning the data 
-    public String Data()
-    {
-        String dataSet = "Your bet type is " + betType
-                        + "Your bet amount is " + betAmount
-                        + "Your playing numbers are " + playerNumbers
-                        + "The winning number is " + winningNumber;
+    public String getData() {
+        String dataSet = "\n\nYour bet type is: " + betType
+                + String.format("\n\nYour bet amount is: $%2f", betAmount) //fix the format to leave just 2 digits
+                + "\n\nYour playing numbers are " + playerNumbers
+                + "\n\nThe winning number is " + winningNumber;
         return dataSet;
     }
+
+    /*
+    public void BetType()
+    {
+        switch (betType)
+        {
+        case "Straight":
+            
+        }
+    }*/
+    //all the coputations to determine if the person won or lost
+    public double GuessResult() {
+        if ("Straight".equals(betType)) {
+            if (playerNumbers / winningNumber == 1) //or % = 0 
+            {
+                betType = "Straight";
+                amountEarned = 600 * betAmount;    //player wins $600 for each $1 wagered
+            } else {
+                System.out.println("Sorry, you lost. Try again next time!");
+            }
+        } else // Box bet
+        {
+            if ("all three numbers match in any order" or  or )
+            {
+                //player wins
+                if ("player’s number contains duplicates") {
+                    amountEarned = 200 * betAmount;    //player wins $200 for each $1 wagered
+                } else {
+                    amountEarned = 100 * betAmount;    //player wins $100 for each $1 wagered
+                }
+            }
+            else
+            {
+                System.out.println("Sorry, you lost. Try again next time!");     
+                    }
+        }
+
+    }
+
 }
--- mySixthAssignment/src/NumbersGameTest.java
+++ mySixthAssignment/src/NumbersGameTest.java
@@ -23,21 +23,27 @@ public class NumbersGameTest {
         int winningNumbers;
         String input;
     
-        String betType = JOptionPane.showInputDialog("Select your bet type. \nIt can be either \"Straight\" or \"Box\" ");
+        String betType = JOptionPane.showInputDialog("Select your bet type. \n\nIt can be either \"Straight\" or \"Box\" ");
         
         input = JOptionPane.showInputDialog("Insert your bet amount");
         
         betAmount = Double.parseDouble(input);
         
-        input = JOptionPane.showInputDialog("Insert your player numbers");
+        input = JOptionPane.showInputDialog("Insert your player numbers. \n3-digit number");
         
         playerNumbers = Integer.parseInt(input);
         
-        input = JOptionPane.showInputDialog("Insert the winning numbers");
+        input = JOptionPane.showInputDialog("Insert the winning numbers. \n3-digit number");
         
         winningNumbers = Integer.parseInt(input);
         
+        //game
+        NumbersGame myGame = new NumbersGame(betType, betAmount, playerNumbers, winningNumbers);
         
+        
+        //getting all the data
+        System.out.println("Here is your information:" + myGame.getData());
     }
+    
     
 }
