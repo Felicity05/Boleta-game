3c28e1d722aa61cd6d679e7abc1564bdc42dd53f
Felicity05 <arelysalvarezpadilla@Arelyss-MacBook-Air.local>
2017-03-09 10:13:43 -0500

    code complete

--- mySixthAssignment/src/NumbersGame.java
+++ mySixthAssignment/src/NumbersGame.java
@@ -16,7 +16,6 @@ public class NumbersGame {
     private int playerNumbers;
     private int winningNumber;
     private double amountEarned;
-    
 
     public NumbersGame(String betType, double betAmount, int playerNumbers, int winningNumber) {
         this.betType = betType;
@@ -29,55 +28,39 @@ public class NumbersGame {
     public String getData() {
         String dataSet = "\n\nYour bet type is: " + betType
                 + String.format("\n\nYour bet amount is: $%.2f", betAmount) //fix the format to leave just 2 digits
-                + "\n\nYour playing numbers are " + playerNumbers
-                + "\n\nThe winning number is " + winningNumber;
+                + "\n\nYour playing number is: " + playerNumbers
+                + "\n\nThe winning number is: " + winningNumber;
         return dataSet;
     }
 
-    //determine bet type
-    public boolean IsStraight()
-    {
-        return false;
-    }
-    
-     
     //determine whether there is an “exact order” match
-    public boolean IsExactOrder()
-    {
+    public boolean IsExactOrder() {
         boolean exactOrder;
         exactOrder = (playerNumbers == winningNumber);
         return exactOrder;
     }
-    
-    
+
     //determine whether there is an “any order” match with different numbers
-   public boolean IsAnyOrder()
-    {
-       boolean anyOrder = (playerNumbers == winningNumber||
-                    playerNumbers/100 == winningNumber/100 ||
-                    playerNumbers%10 == winningNumber%10 ||
-                   (playerNumbers/10)%10 == (winningNumber/10)%10 ||
-                    playerNumbers%10 == winningNumber/100 ||
-                    playerNumbers%10 == (winningNumber/10)%10);
-        return anyOrder;
-    }
-    
-    //determine whether a Box bet contains duplicate numbers
-    public boolean HasDuplicates()
-    {
+    public boolean IsAnyOrder() {
         //private int firstDigit = playerNumbers/100;
-      // private int secondDigit = (playerNumbers/10)%10;
-      //private int thirdDigit = playerNumbers/10;
-        boolean duplicates = (playerNumbers/10)%10 == playerNumbers%10 ||
-                              playerNumbers/100 == playerNumbers%10 ||
-                              playerNumbers/100 == (playerNumbers/10)%10;
+        // private int secondDigit = (playerNumbers/10)%10;
+        //private int thirdDigit = playerNumbers%10;
+        boolean order1 = playerNumbers == winningNumber;
+        boolean order2 = playerNumbers / 100 == winningNumber / 100 && ((playerNumbers % 10 == (winningNumber / 10) % 10)) && (playerNumbers / 10) % 10 == winningNumber % 10;
+        boolean order3 = playerNumbers % 10 == winningNumber / 100 && playerNumbers / 100 == (winningNumber / 10) % 10 && (playerNumbers / 10) % 10 == winningNumber % 10;
+        boolean order4 = playerNumbers % 10 == winningNumber / 100 && (playerNumbers / 10) % 10 == (winningNumber / 10) % 10 && playerNumbers / 100 == winningNumber / 100;
+        boolean order5 = playerNumbers / 100 == (winningNumber / 10) % 10 && (playerNumbers / 10) % 10 == winningNumber / 100 && playerNumbers % 10 == winningNumber % 10;
+        boolean order6 = playerNumbers / 100 == winningNumber % 10 && (playerNumbers / 10) % 10 == winningNumber / 100 && playerNumbers % 10 == (winningNumber / 10) % 10;
+        boolean anyOrder;
+        return anyOrder = order1 || order2 || order3 || order4 || order5 || order6;
+    }
+
+    //determine whether a Box bet contains duplicate numbers
+    public boolean HasDuplicates() {
+        boolean duplicates = ((playerNumbers / 10) % 10 == playerNumbers % 10)
+                || (playerNumbers / 100 == playerNumbers % 10)
+                || (playerNumbers / 100 == (playerNumbers / 10) % 10);
         return duplicates;
     }
-   
 
-    }
-
-    
-    
-    
-    
+}
--- mySixthAssignment/src/NumbersGameTest.java
+++ mySixthAssignment/src/NumbersGameTest.java
@@ -6,7 +6,6 @@ import javax.swing.JOptionPane;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author arelysalvarezpadilla
@@ -17,73 +16,58 @@ public class NumbersGameTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         double betAmount;
         int playerNumbers;
         int winningNumbers;
         double amountEarned;
         String input;
-    
+
         String betType = JOptionPane.showInputDialog("Select your bet type. \n\nIt can be either \"Straight\" or \"Box\" ");
-        
+
         input = JOptionPane.showInputDialog("Insert your bet amount");
-        
+
         betAmount = Double.parseDouble(input);
-        
-        input = JOptionPane.showInputDialog("Insert your player numbers. \n3-digit number");
-        
+
+        input = JOptionPane.showInputDialog("Insert your player number. \n3-digit number");
+
         playerNumbers = Integer.parseInt(input);
-        
-        input = JOptionPane.showInputDialog("Insert the winning numbers. \n3-digit number");
-        
+
+        input = JOptionPane.showInputDialog("Insert the winning number. \n3-digit number");
+
         winningNumbers = Integer.parseInt(input);
-        
-      
-        string       
-        boolean goodNumb = String.format("%3d", input);
-        
-        
+
         //game
         NumbersGame myGame = new NumbersGame(betType, betAmount, playerNumbers, winningNumbers);
-        
-        
+
         //getting all the data
         System.out.println("Here is your information:" + myGame.getData());
-        
-          
-        
-        
+
         //all the tests to see if the player win or lose 
-        if (myGame.IsStraight()) //boolean method
+        if ("Straight".equals(betType) || "straight".equals(betType)) //boolean method
         {
             if (myGame.IsExactOrder()) {
-                betType = "Straight";
-                amountEarned = 600 * betAmount;    //player wins $600 for each $1 wagered
-                System.out.printf("\nCongratulations, You win!!! \n\nThe amount you earned is: $%.2f ", amountEarned);
+                amountEarned = betAmount * 600;    //player wins $600 for each $1 wagered
+                System.out.printf("\nYour numbers are exactly the same ;) Congratulations, You won!!! \n\nThe amount you earned is: $%.2f ", amountEarned);
             } else {
-                System.out.println("\nSorry, you lost. Try again next time!");
+                System.out.println("\nSorry, you lost :( Try again next time!");
             }
-        } else // Box bet
+        } else if ("Box".equals(betType) || "box".equals(betType))// Box bet
         {
-            if (myGame.IsAnyOrder())
-            {
-                betType = "Box";
-                if (myGame.HasDuplicates())
-                {
-                amountEarned = betAmount * 200;
-                System.out.printf("\nCongratulations, You win!!! \n\nThe amount you earned is: $%.2f ", amountEarned);
+            if (myGame.IsAnyOrder()) {
+                if (myGame.HasDuplicates()) {
+                    amountEarned = betAmount * 200;
+                    System.out.printf("\nYou have a duplicate. Congratulations, you won!!! \n\nThe amount you earned is: $%.2f ", amountEarned);
+                } else {
+                    amountEarned = betAmount * 100;
+                    System.out.printf("\nCongratulations, You won!!! \n\nThe amount you earned is: $%.2f ", amountEarned);
                 }
-                else 
-                {
-                amountEarned = betAmount * 100;
-                System.out.printf("\nCongratulations, You win!!! \n\nThe amount you earned is: $%.2f ", amountEarned);   
-                }
-            }
-            else 
-            {
+            } else {
                 System.out.println("\nYou are not lucky today :( Sorry. Try again next time!");
             }
+        } else {
+            System.out.println("\nThat's not a bet type. Lo siento, Insert your bet type again");
         }
     }
-   
+
 }
